555516c88591efd13f2f3c9c861cfb588bd353a4
Gekkoinabox <36261451+Gekkoinabox@users.noreply.github.com>
2018-05-08 02:09:48 +0100

    Moved to github

---
+++ SnowboardingGame/SnowboardGame/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+   // CharacterController m_CharacterController;
+
+    private void Start()
+    {
+       // m_CharacterController = GetComponent<CharacterController>;
+    }
+
+    private void Update()
+    {
+        
+    }
+
+     private void FixedUpdate() //Used for physics updates
+    {
+        
+    }
+}
+
